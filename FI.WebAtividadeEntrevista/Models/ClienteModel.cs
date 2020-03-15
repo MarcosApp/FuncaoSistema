004a50e21278eb17516cc1300be23693c2b9ef6d
MarcosApps <marcosdossantos43@hotmail.com>
2020-03-14 22:18:11 -0300

    Commit

---
+++ FI.WebAtividadeEntrevista/Models/ClienteModel.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebAtividadeEntrevista.Models
+{
+    /// <summary>
+    /// Classe de Modelo de Cliente
+    /// </summary>
+    public class ClienteModel
+    {
+        public long Id { get; set; }
+        
+        /// <summary>
+        /// CEP
+        /// </summary>
+        [Required]
+        [MaxLength(9)]
+        public string CEP { get; set; }
+
+        /// <summary>
+        /// Cidade
+        /// </summary>
+        [Required]
+        [MaxLength(50)]
+        public string Cidade { get; set; }
+
+        /// <summary>
+        /// E-mail
+        /// </summary>
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage = "Digite um e-mail válido")]
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Estado
+        /// </summary>
+        [Required]
+        [MaxLength(2)]
+        public string Estado { get; set; }
+
+        /// <summary>
+        /// Logradouro
+        /// </summary>
+        [Required]
+        [MaxLength(500)]
+        public string Logradouro { get; set; }
+
+        /// <summary>
+        /// Nacionalidade
+        /// </summary>
+        [Required]
+        [MaxLength(50)]
+        public string Nacionalidade { get; set; }
+
+        /// <summary>
+        /// Nome
+        /// </summary>
+        [Required]
+        [MaxLength(50)]
+        public string Nome { get; set; }
+
+        /// <summary>
+        /// Sobrenome
+        /// </summary>
+        [Required]
+        [MaxLength(255)]
+        public string Sobrenome { get; set; }
+
+        /// <summary>
+        /// Telefone
+        /// </summary>
+        [MaxLength(14)]
+        public string Telefone { get; set; }
+        /// <summary>
+        /// CPF
+        /// </summary>
+        [Required]
+        [MaxLength(14)]
+        public string CPF { get; set; }
+    }    
+}
